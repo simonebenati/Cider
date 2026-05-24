3e567a5d03511c8abd6c18c969b9f6cb4437250a
Simone <simone.benati@outlook.com>
2026-05-24 16:19:10 +0200

    Introduced stream mapping to memory model

--- main.c
+++ main.c
@@ -8,8 +8,9 @@
 typedef struct {
     uint8_t file_open_t; // Number of (t)times file is opened
     FILE *file_ptr;
-    size_t line_counter;
+    size_t file_size;
     size_t line_size;
+    size_t line_no;
     char *line;
     size_t *line_offset; 
 } FileStruct;
@@ -23,7 +24,7 @@ void file_track_open(FileStruct *f_struct) {
 void file_track_close(FileStruct *f_struct) {
     if (f_struct->file_open_t == 0) return;
     f_struct->file_open_t--;
-    if (f_struct == 0) free(f_struct);
+    if (f_struct-> file_open_t == 0) free(f_struct);
     return;
 }
 
@@ -43,41 +44,84 @@ size_t file_get_size (FILE* file) {
     return size;
 }
 
-FileStruct* file_to_struct(FILE* file) {
+size_t* file_get_lines_and_size (FILE* file) {
+    char *l = 0;
+    size_t n = 0;
+    size_t ln = 0;
+    size_t ret = 0;
+    // stats[0] holds size stats[1] holds lines
+    size_t *stats = malloc(2*(sizeof(size_t*)));
+    if (stats == NULL) {
+        perror("Memory allocation failed during file_get_lines_and_size()");
+        exit(1);
+    }
+    stats[0] = 0;
+    stats[1] = 0;
+
+    while((ret = getline(&l, &n, file)) != -1) {
+        stats[0] += ret;
+        stats[1]++;
+    }
+    rewind(file);
+
+    return stats;
+}
+
+/* 
+Safely initialize struct FileStruct 
+First allocates memory to represent the struct.
+Second sets values to non-garbage
+Lastly allocates memory for directly accessible buffer for text and array of size_t 
+*/
+FileStruct* file_struct_init(FILE *file) {
     FileStruct *f_struct = malloc(sizeof(FileStruct));
     if (f_struct == NULL) {
         printf("Something went wrong when allocating memory for lines..");
         exit(1);
     }
-
+    size_t *f_stats = file_get_lines_and_size(file);
     f_struct->file_open_t = 0;
-    file_track_open(f_struct);
     f_struct->file_ptr = file;
-    f_struct->line_counter = 0;
-    size_t *l_ctr = &f_struct->line_counter;
-    f_struct->line_size = 1024;
-    f_struct->line = malloc(f_struct->line_size);
-    f_struct->line_offset = malloc(sizeof(size_t));
+    f_struct->file_size = f_stats[0];
+    f_struct->line_size = f_stats[0]+f_stats[1];
+    f_struct->line_no = f_stats[1];
+    f_struct->line = malloc(f_struct->line_size + f_struct->line_no);
+    f_struct->line_offset = malloc(sizeof(size_t*) * f_struct->line_no);
 
+    if (f_struct->line == NULL || f_struct->line_offset == NULL) {
+        printf("Something went wrong when allocating memory for lines..");
+        free(f_struct->line);
+        free(f_struct->line_offset);
+        free(f_struct);
+        free(f_stats);
+        exit(1);
+    }
+
+    *f_struct->line_offset = 0;
+    file_track_open(f_struct);
+
+    free(f_stats);
+
+    return f_struct;
+}
+
+FileStruct* file_to_struct(FILE *file) {
+    
+    FileStruct *f_struct = file_struct_init(file);
     size_t alloc_bytes = 0;
     size_t ret = 0;
     size_t offset = 0;
+    size_t i = 0;
     char *l = NULL;
+
     while((ret = getline(&l, &alloc_bytes, file)) != -1) {
-        if (f_struct->line_size - offset < ret) {
-            while(f_struct->line_size - offset < ret) {
-                f_struct->line_size *= 2;
-                f_struct->line = realloc(f_struct->line, f_struct->line_size);
-            }
-        }
-        memcpy(&f_struct->line[offset], l, ret);
-        f_struct->line_offset[*l_ctr] = offset;
+        memcpy(&f_struct->line[offset], l, ret+1);
+        f_struct->line_offset[i] = offset;
         offset += ret+1;
-        f_struct->line[offset-1] = '\0';
-        (*l_ctr)++;
-        f_struct->line_offset = realloc(f_struct->line_offset, sizeof(size_t) * (*l_ctr + 1));
+        ++i;
     }
 
+    free(l);
     return f_struct;
 }
 
@@ -120,9 +164,11 @@ void file_print(FILE* file) {
 
 int main () {
     FileStruct *f = file_open("./File.txt", "r");
-    
-    //file_print(f->file_ptr);
-    printf("%s", &f->line[f->line_offset[0]]);
+
+    printf("Line %s", &f->line[f->line_offset[4]]);
+    fflush(stdout);
+
+    free(f);
 
     return 0;
 }
