47cbc000b49babb818066529b60eec7d1b0a52ac
Simone <simone.benati@outlook.com>
2026-06-21 12:09:01 +0200

    Pushing some progresses

--- main.c
+++ main.c
@@ -6,12 +6,14 @@
 #include <string.h>
 
 typedef struct {
+    size_t cursor_x;
+    size_t cursor_y;
     uint8_t file_open_t; // Number of (t)times file is opened
     FILE *file_ptr;
     size_t file_size;
-    size_t line_size;
+    size_t line_size; // This is a metadata to track buffer size when file is opened or closed
     size_t line_no;
-    char *line;
+    char *line_buffer; // This is the pointer to the buffer which contains actual file lines
     size_t *line_offset; 
 } FileStruct;
 
@@ -44,6 +46,7 @@ size_t file_get_size (FILE* file) {
     return size;
 }
 
+/* Function to get metadata as number of lines in file and size of file */
 size_t* file_get_lines_and_size (FILE* file) {
     char *l = 0;
     size_t n = 0;
@@ -80,17 +83,19 @@ FileStruct* file_struct_init(FILE *file) {
         exit(1);
     }
     size_t *f_stats = file_get_lines_and_size(file);
+    f_struct->cursor_x = 0;
+    f_struct->cursor_y = 0;
     f_struct->file_open_t = 0;
     f_struct->file_ptr = file;
     f_struct->file_size = f_stats[0];
     f_struct->line_size = f_stats[0]+f_stats[1];
     f_struct->line_no = f_stats[1];
-    f_struct->line = malloc(f_struct->line_size + f_struct->line_no);
+    f_struct->line_buffer = malloc(f_struct->line_size + f_struct->line_no);
     f_struct->line_offset = malloc(sizeof(size_t*) * f_struct->line_no);
 
-    if (f_struct->line == NULL || f_struct->line_offset == NULL) {
+    if (f_struct->line_buffer == NULL || f_struct->line_offset == NULL) {
         printf("Something went wrong when allocating memory for lines..");
-        free(f_struct->line);
+        free(f_struct->line_buffer);
         free(f_struct->line_offset);
         free(f_struct);
         free(f_stats);
@@ -105,6 +110,8 @@ FileStruct* file_struct_init(FILE *file) {
     return f_struct;
 }
 
+/* Safely map opened file to struct with additional metadata 
+in order to manipulate file later */
 FileStruct* file_to_struct(FILE *file) {
     
     FileStruct *f_struct = file_struct_init(file);
@@ -115,7 +122,7 @@ FileStruct* file_to_struct(FILE *file) {
     char *l = NULL;
 
     while((ret = getline(&l, &alloc_bytes, file)) != -1) {
-        memcpy(&f_struct->line[offset], l, ret+1);
+        memcpy(&f_struct->line_buffer[offset], l, ret+1);
         f_struct->line_offset[i] = offset;
         offset += ret+1;
         ++i;
@@ -140,6 +147,21 @@ FileStruct* file_open(const char *path, const char *perms) {
     return f_struct;
 }
 
+// size_t* pos_calculator(FileStruct *f_struct, char *key) {
+//     if (*key == '')
+// }
+
+void struct_print(FileStruct* f_struct) {
+    size_t n_lines = f_struct->line_no;
+    for (int i = 0; i < n_lines; ++i) {
+        printf("%s", &f_struct->line_buffer[f_struct->line_offset[i]]);
+    }
+    printf("\n");
+    printf("\033[%d;%dH", 1, 1);
+
+    return;
+}
+
 /* Takes a file into input and safely prints its content.
 It checks for size first and measures if bytes printed match file dimension. */
 void file_print(FILE* file) {
@@ -162,12 +184,38 @@ void file_print(FILE* file) {
     return;
 }
 
+/* Instanciate a buffer for editing content of file. */
+// char* load_buf(FileStruct* f_struct) {
+//     char *edit_buffer = malloc(f_struct->line_size);
+//     if (edit_buffer == NULL) {
+//         printf("Something went wrong when opening the file..");
+//         exit(1);
+//     }
+
+//     memcpy(edit_buffer, f_struct->line_buffer, f_struct->line_size);
+//     if (edit_buffer == NULL)
+//         exit(1);
+
+//     return edit_buffer;
+// }
+
 int main () {
     FileStruct *f = file_open("./File.txt", "r");
 
-    printf("Line %s", &f->line[f->line_offset[4]]);
-    fflush(stdout);
+    // printf("Line %s", &f->line_buffer[f->line_offset[4]]);
+    // fflush(stdout);
+    struct_print(f);
+    int y = 0;
+    while((y = getchar()) > 0) {
+        if (y == 10) continue;
+        if (y == 48) y = 0;
+        if (y == 49) y = 1;
+        if (y == 50) y = 2;
+        if (y == 51) y = 3;
+        if (y == 52) y = 4;
 
+        printf("\nPrinted line: %s", &f->line_buffer[f->line_offset[y]]);
+    }
     free(f);
 
     return 0;
